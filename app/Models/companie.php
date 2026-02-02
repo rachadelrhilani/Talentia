c5bdfc544d67381271d14a74359e8728289d2b0a
rachad <rachdos76@gmail.com>
2026-02-02 12:48:27 +0100

    creer le modal companie avec leurs relations

--- app/Models/companie.php
+++ app/Models/companie.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class companie extends Model
 {
-    //
+    protected $fillable = ['user_id','name','description','logo'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function jobOffers()
+    {
+        return $this->hasMany(job_offer::class);
+    }
 }
