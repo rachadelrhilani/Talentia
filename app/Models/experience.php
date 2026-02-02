9ea86005d4e234d0651e4f049533b049ce703d79
rachad <rachdos76@gmail.com>
2026-02-02 12:45:21 +0100

    creer le modal experience avec leurs relations

--- app/Models/experience.php
+++ app/Models/experience.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class experience extends Model
 {
-    //
+    protected $fillable = ['profile_id','position','company','start_date','end_date'];
+
+    public function profile()
+    {
+        return $this->belongsTo(Profile::class);
+    }
 }
